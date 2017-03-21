667bfbfa69e7b17cc999faffcedde086eedbfb4b
EyesOfRinn <contact@eyesofrinn.com>
2017-03-21 17:40:44 +0400

    Optimized The Code
    Optimized the code.

--- src/example/Example.java
+++ src/example/Example.java
@@ -8,18 +8,13 @@ import xyz.rinn.uuidgrabber.UUIDGrabber;
 
 public class Example {
 	public static void main(String[] args) {
-	    //Create a new instance of UUIDGrabber
-		UUIDGrabber grabber = new UUIDGrabber("Shoot");
-		//Create a list of UUIDs
-		List<UUID> uuids = null;
+	    List<UUID> uuids = null;
 		
 		try {
-		    //Get all of the UUIDs
-			uuids = grabber.call();
+			uuids = new UUIDGrabber("Shoot").call();
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}
-		//Print your response, in this case, it is "All of the UUIDs for Shoot are: [afa1ce8a-7f6c-4018-aa1c-f1f92fff1704]"
-		System.out.println("All of the UUIDs for Shoot are: " + uuids);
+		uuids.forEach(uuid -> System.out.println(uuid));
 	}
 }
--- src/main/java/xyz/rinn/uuidgrabber/UUIDGrabber.java
+++ src/main/java/xyz/rinn/uuidgrabber/UUIDGrabber.java
@@ -16,42 +16,46 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class UUIDGrabber implements Callable<List<UUID>> {
-    private String name;
+    private final String name;
 
-    public UUIDGrabber(String name) {
+    public UUIDGrabber(final String name) {
     	this.name = name;
     }
     
     public List<UUID> call() throws IOException, ParseException {
-        List<UUID> list = new ArrayList<>();
+        final List<UUID> list = new ArrayList<>();
         
-        //Create a connection
-        HttpURLConnection connection = (HttpURLConnection) new URL("https://api.mojang.com/profiles/minecraft").openConnection();
+        // Create a connection
+        final HttpURLConnection connection = (HttpURLConnection) new URL("https://api.mojang.com/profiles/minecraft").openConnection();
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/json");
         connection.setUseCaches(false);
         connection.setDoInput(true);
         connection.setDoOutput(true);
         
-        //Write to the connection
-        OutputStream stream = connection.getOutputStream();
-        stream.write(new String("[\"" + name + "\"]").getBytes());
+        // Write to the connection
+        final OutputStream stream = connection.getOutputStream();
+        stream.write(new String("[\"" + this.name + "\"]").getBytes());
+        
+        // Flush and close the output stream
         stream.flush();
         stream.close();
         
-        //Convert the response into an array of objects
-        JSONArray array = (JSONArray) new JSONParser().parse(new InputStreamReader(connection.getInputStream()));
+        // Convert the response into an array of objects
+        final JSONArray array = (JSONArray) new JSONParser().parse(new InputStreamReader(connection.getInputStream()));
         
-        //Loop through each profile in the array
-        for (Object profile : array) {
-            JSONObject jsonProfile = (JSONObject) profile;
-            //UUIDs returned do not have hyphens('-') in them, so we must add them in order to convert a String to a UUID
-            list.add(getUUID(jsonProfile.get("id").toString()));
+        // Loop through each profile in the array
+        for (final Object profile : array) {
+            // UUIDs returned do not have hyphens('-') in them, so we must add them in order to convert a String to a UUID
+            list.add(this.getUUID(((JSONObject) profile).get("id").toString()));
         }
         return list;
     }
 
     private UUID getUUID(String id) {
-        return UUID.fromString(id.substring(0, 8) + "-" + id.substring(8, 12) + "-" + id.substring(12, 16) + "-" + id.substring(16, 20) + "-" +id.substring(20, 32));
+    	final StringBuilder string = new StringBuilder();
+    	string.append(id.substring(0, 8)).append('-').append(id.substring(8, 12)).append('-').append(id.substring(12, 16)).append('-').append(id.substring(16, 20)).append('-').append(id.substring(20, 32));
+    	
+        return UUID.fromString(string.toString());
     }
 }
