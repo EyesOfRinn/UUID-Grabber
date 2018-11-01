82b7e9170877c602d2cda612d375db2f01eb4647
EyesOfRinn <EyesOfRinn@users.noreply.github.com>
2018-11-02 02:03:44 +0400

    Update src/main/java/xyz/rinn/uuidgrabber/utils/UUIDUtils.java

--- src/main/java/xyz/rinn/uuidgrabber/utils/UUIDUtils.java
+++ src/main/java/xyz/rinn/uuidgrabber/utils/UUIDUtils.java
@@ -3,7 +3,7 @@ package xyz.rinn.uuidgrabber.utils;
 import java.util.UUID;
 
 public class UUIDUtils {
-	public static UUID getUUID(final String id) {
+    public static UUID getUUID(final String id) {
         return UUID.fromString(id.substring(0, 8) + '-' + id.substring(8, 12) + '-' + id.substring(12, 16) + '-' + id.substring(16, 20) + '-' + id.substring(20, 32));
     }
 }
